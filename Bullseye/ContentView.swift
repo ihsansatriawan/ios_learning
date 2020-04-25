60f36020d1ccdb2086cd0ccbc1f965033edd2786
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2020-04-25 15:54:35 +0700

    Adding button and show alert when button clicled

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Welcome to my first app!")
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+            Button(action: {
+                print("Button pressed!")
+                self.alertIsVisible = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $alertIsVisible) { () ->
+                Alert in
+                return Alert(title: Text("My first app!"), message: Text("Body aja"), dismissButton: .default(Text("Tutup")))
+            }
+        }
     }
 }
 
