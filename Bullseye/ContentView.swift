3d6e0606ad87fea29ad12e08686454d8b4344f06
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2020-04-25 12:48:19 +0700

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Ihsan on 25/04/20.
+//  Copyright © 2020 Ihsan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
